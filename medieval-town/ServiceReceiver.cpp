0ed19010b3c8d7cdce7511919938f1101bb7a04d
IrealCreation <49712202+IrealCreation@users.noreply.github.com>
2025-09-24 17:00:16 +0200

    Calcul de l'attractivité liée aux services

--- medieval-town/ServiceReceiver.cpp
+++ medieval-town/ServiceReceiver.cpp
@@ -1,5 +1,8 @@
 #include "ServiceReceiver.h"
 #include "Building.h"
+#include <algorithm>
+using std::map;
+using std::find;
 
 
 namespace Models
@@ -21,10 +24,61 @@ namespace Models
 	void ServiceReceiver::addService(Service service, Building* building)
 	{
 		serviceBuildings[service] = building;
+		this->updateAttractiveness();
 	}
 	void ServiceReceiver::removeService(Service service)
 	{
 		serviceBuildings.erase(service);
+		this->updateAttractiveness();
+	}
+
+	float ServiceReceiver::getAttractiveness(Pop pop) const
+	{
+		return attractiveness.find(pop)->second;
+	}
+
+	void ServiceReceiver::updateAttractiveness()
+	{
+		map<Pop, float> scoreByPop = {
+			{ Pop::Gueux, 0.0f },
+			{ Pop::Bourgeois, 0.0f },
+			{ Pop::Noble, 0.0f }
+		};
+
+		if (serviceBuildings.empty()) {
+			// Il n'y a aucun bâtiment de service qui nous dessert : l'attractivité est de 0
+			attractiveness = scoreByPop;
+			return;
+		}
+
+		// On parcourt tous les bâtiments de service
+		for(const auto& service_building : serviceBuildings) 
+		{
+			// Pour chaque service desservi, on regarde chaque pop afin de vérifier si elle désire ce service
+			for (const auto& pop_services : popServices)
+			{
+				// Est-ce que le service de ce bâtiment est présent dans la liste de services de cette population ?
+				if (find(pop_services.second.begin(), pop_services.second.end(), service_building.first) != pop_services.second.end()) 
+				{
+					// Si oui, on accroit le score d'attractivité pour cette pop de 1
+					scoreByPop[pop_services.first]++;
+				}
+			}
+		}
+
+		// On divise le score d'attractivité pour chaque pop par la quantité de services que cette pop requiert
+		for(auto& pop_score : scoreByPop) 
+		{
+			// Pas besoin de diviser si le score est à 0
+			if (pop_score.second == 0.0f)
+				continue;
+
+			auto it = popServices.find(pop_score.first);
+			pop_score.second /= static_cast<float>(it->second.size());
+		}
+
+		// On attribue les nouveaux score d'attractivité
+		attractiveness = scoreByPop;
 	}
 
 }
--- medieval-town/ServiceReceiver.h
+++ medieval-town/ServiceReceiver.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <map>
+#include <vector>
 #include "Service.h"
+#include "Pop.h"
 
 namespace Models
 {
@@ -14,16 +16,35 @@ namespace Models
 	public:
 		ServiceReceiver();
 
+		const std::map<Pop, std::vector<Service>> popServices = {
+			{
+				Pop::Gueux,
+				{ Service::Priere, Service::Eau, Service::Garde, Service::Social, Service::Picole }
+			},
+			{
+				Pop::Bourgeois,
+				{ Service::Priere, Service::Monachisme, Service::Eau, Service::Linge, Service::Soin, Service::Garde, Service::Chatiment, Service::Social, Service::Diner, Service::Comedie }
+			},
+			{
+				Pop::Noble,
+				{ Service::Priere, Service::Monachisme, Service::Etude, Service::Eau, Service::Linge, Service::Soin, Service::Bain, Service::Garde, Service::Chatiment, Service::Justice, Service::Social, Service::Diner, Service::Comedie, Service::Art, Service::Plaisir }
+			}
+		};
+
 		Building* getServiceBuilding(Service service) const; // Retourne le bâtiment de service desservant cet endroit pour le service donné ; nullptr si pas de bâtiment
 		std::map<Service, Building*> getAllServiceBuildings() const; // Retourne la liste des bâtiments de service desservant cet endroit
 
 		void addService(Service service, Building* building); // Ajoute un bâtiment de service desservant cet endroit
 		void removeService(Service service); // Retire le bâtiment de service desservant cet endroit pour le service donné
 
-		// TODO: attractivité
+		float getAttractiveness(Pop pop) const; // Retourne l'attractivité pour une population en fonction des services disponibles
 
-	protected:
-		std::map<Service, Building*> serviceBuildings; // Bâtiments de service desservant cet endroit
+	private:
+		std::map<Service, Building*> serviceBuildings; // Bâtiments de service desservant cet endroit. Portée privée car toute modification doit passer par les setters afin que soit appelé updateAttractiveness()
+
+		std::map<Pop, float> attractiveness; // Attractivité pour une population en fonction des services disponibles
+
+		void updateAttractiveness(); // Met à jour l'attractivité après que les services disponibles aient évolués
 	};
 }
 
